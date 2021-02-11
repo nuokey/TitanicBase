f1382caa61879a67c5881855c7a67090a81e38f9
Timo <66961498+nuokey@users.noreply.github.com>
2021-02-11 15:11:18 +0300

    Update

--- main.py
+++ main.py
@@ -1,26 +1,31 @@
 import sqlite3
 import csv
 
-data = sqlite3.connect('data.db')
-cursor = data.cursor()
-try:
-	cursor.execute('''CREATE TABLE people (
-		name TEXT,
-		age TEXT,
-		job TEXT,
-		survived TEXT
-	)''')
-except:
-	pass
+def create_database():
+	data = sqlite3.connect('data.db')
+	cursor = data.cursor()
+	try:
+		cursor.execute('''CREATE TABLE people (
+			name TEXT,
+			age TEXT,
+			job TEXT,
+			survived TEXT
+		)''')
+	except:
+		pass
 
-with open('titanic.csv') as csvfile:
-	titanic = csv.reader(csvfile)
-	for line in titanic:
-		if line != ['Name', 'Age', 'Class/Dept', 'Fare', 'Joined', 'Job', 'Survived']:
-			try:
-				cursor.execute(f'INSERT INTO people VALUES ("{line[0]}", "{line[1]}", "{line[5]}", "{line[6]}")')
-			except:
-				pass
+	with open('titanic.csv') as csvfile:
+		titanic = csv.reader(csvfile)
+		for line in titanic:
+			if line != ['Name', 'Age', 'Class/Dept', 'Fare', 'Joined', 'Job', 'Survived']:
+				print(line)
+				try:
+					cursor.execute(f'INSERT INTO people VALUES ("{line[0]}", "{line[1]}", "{line[5]}", "{line[6]}")')
+				except:
+					pass
+
+create_database()
+
 survived = 0
 not_survived = 0
 for i in cursor.execute('SELECT survived FROM people'):
